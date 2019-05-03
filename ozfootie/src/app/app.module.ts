be205affd4d90676ce7e56eb678c8d2d812bd2fa
Ravi Rathore <rpsr15@Ravis-MacBook-Pro.local>
2019-05-03 18:21:00 +1000

    added navbar-cmp

--- ozfootie/src/app/app.module.ts
+++ ozfootie/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { NavbarCmpComponent } from './navbar-cmp/navbar-cmp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    NavbarCmpComponent
   ],
   imports: [
     BrowserModule,
---
+++ ozfootie/src/app/navbar-cmp/navbar-cmp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-cmp',
+  templateUrl: './navbar-cmp.component.html',
+  styleUrls: ['./navbar-cmp.component.css']
+})
+export class NavbarCmpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
