0e22528a0394502cbe8d6d30853209416bdfdbfc
unknown <ravipratapsinghrathore15@gmail.com>
2019-05-11 18:37:58 +1000

    added news headlines

--- ozfooty/src/app/services/data.service.ts
+++ ozfooty/src/app/services/data.service.ts
@@ -4,8 +4,30 @@ import {Injectable} from '@angular/core';
 @Injectable()
 
 export class DataService {
-    constructor(private httpService: HttpClient)
-    {
 
+    newsUrl = 'https://newsapi.org/v2/top-headlines?country=au&category=sports&apiKey=bfcd98dce90244bd8d79b0e7f3b50511&q=afl';
+    headlines: string[];
+    constructor(private httpService: HttpClient) {
+
+    }
+
+    getNews() {
+        const promise = new Promise(
+            (resolve) => {
+                this.httpService.get(this.newsUrl).subscribe(
+                    (data: any) => {
+                        const news = [];
+                        const articles  = data.articles;
+                        // tslint:disable-next-line: forin
+                        for (const index in articles) {
+
+                         news.push(articles[index].title);
+                        }
+                        resolve(news);
+                    }
+                );
+             }
+        );
+        return promise;
     }
 }
