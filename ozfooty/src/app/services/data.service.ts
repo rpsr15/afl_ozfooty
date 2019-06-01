25e48e2f4fbc48aa65f7b491007d9348608bceac
rpsr15 <ravipratapsinghrathore15@gmail.com>
2019-06-01 15:21:52 +1000

    added methods to dta service to support getnearyby component
    	modified:   src/app/games-nearby/games-nearby.component.css
    	modified:   src/app/games-nearby/games-nearby.component.html
    	modified:   src/app/services/data.service.ts

--- ozfooty/src/app/services/data.service.ts
+++ ozfooty/src/app/services/data.service.ts
@@ -1,30 +1,94 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {Game} from '../model/game.model';
-import {Team} from '../model/team.model';
-import {LadderItem} from '../model/ladder-item.model';
-import {Tip} from '../model/tip.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Game } from '../model/game.model';
+import { Team } from '../model/team.model';
+import { LadderItem } from '../model/ladder-item.model';
+import { Tip } from '../model/tip.model';
 
 @Injectable()
 
 export class DataService {
+  // AFL stadiums and their locations; used to find nearest stadium to the user
+  stadiums = {
+    'Adelaide Oval': { lat: -34.915754, long: 138.596168 }
+    , 'Bellerive Oval': { lat: -42.877208, long: 147.373511 }
+    , 'Carrara': { lat: -28.006634, long: 153.366733 }
+    , 'Eureka': { lat: -37.539436, long: 143.848362 }
+    , 'Gabba': { lat: -27.486301, long: 153.038093 }
+    , 'Jiangwan': { lat: 31.307411, long: 121.518367 }
+    , 'Kardinia Park': { lat: -38.158364, long: 144.354239 }
+    , 'M.C.G.': { lat: -37.820263, long: 144.983052 }
+    , 'Manuka Oval': { lat: -35.318317, long: 149.134533 }
+    , 'Marrara Oval': { lat: -12.398346, long: 130.88681 }
+    , 'Marvel Stadium': { lat: -37.816226, long: 144.946894 }
+    , 'Perth': { lat: -31.95128, long: 115.888671 }
+    , 'Riverway': { lat: -19.317558, long: 146.731451 }
+    , 'S.C.G.': { lat: -33.892066, long: 151.224583 }
+    , 'Sydney Showground': { lat: -33.845694, long: 151.067693 }
+    , 'Traeger Park': { lat: -23.709068, long: 133.874965 }
+    , 'York Park': { lat: -41.426117, long: 147.138717 }
 
+  };
 
-  matches = [];
-  teams: Array<Team> = [];
+  matches = []; // store matches here
+  teams: Array<Team> = []; // teams of the afl
+  // URLs to get data from squibble api
   gameURL = 'https://api.squiggle.com.au/?q=games;year=';
   teamURL = 'https://api.squiggle.com.au/?q=teams';
-
   newsUrl = 'https://newsapi.org/v2/top-headlines?country=au&category=sports&apiKey=bfcd98dce90244bd8d79b0e7f3b50511&q=afl';
   headlines: string[];
   ladderData: Array<LadderItem> = [];
   gamesByRoundAndYear: Array<Game> = [];
   gamesFixtures: Array<Game> = [];
-
   gameTip: Array<Tip> = [];
 
-  constructor(private httpService: HttpClient) {
+  constructor(private httpService: HttpClient) { }
+  //Methods to find nearest stadium to a user
 
+  deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
+  getLatLonFromPostCode(postcode) {
+    const promise = new Promise(
+      (resolve) => {
+        this.httpService.get(`http://open.mapquestapi.com/geocoding/v1/address?key=EBdkGW6MirT15ArDr8bw3Tp0Umv8dJmA&maxResults=1&location=${postcode},australia`).subscribe(
+          (data: any) => {
+            resolve(data.results[0].locations[0].displayLatLng);
+          }
+        );
+      }
+    );
+    return promise;
+  }
+
+  getNearestStadiums(lat, lng) {
+    let minDistance = Number.MAX_VALUE;
+    let nearestStadium = "";
+    for (const i in this.stadiums) {
+      // console.log(this.stadiums[i].lat, this.stadiums[i].long);
+      let distance = this.getDistanceFromLatLonInKm(lat, lng, this.stadiums[i].lat, this.stadiums[i].long);
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestStadium = i;
+
+      }
+    }
+    return nearestStadium;
+
+  }
+  getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the earth in kilometers
+    const dLat = this.deg2rad(lat2 - lat1); // deg2rad below
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in KM
+    return d;
   }
 
   getNews() {
@@ -57,11 +121,7 @@ export class DataService {
       } else {
         this.httpService.get(this.gameURL + year).subscribe(
           (data) => {
-            // this.matches = data;
-            // console.log('DATA-->', data);
-
-
-            resolve(data);
+           resolve(data);
           }
         );
       }
@@ -73,7 +133,7 @@ export class DataService {
   getCurrentRound() {
     const roundUrl = 'https://api.squiggle.com.au/?q=games;year=2019;complete=0';
     const promise = new Promise(
-      (resolve) =>{
+      (resolve) => {
         this.httpService.get(roundUrl).subscribe((data: any) => {
           // console.log('Current ROund ->',data.games[0].round);
 
@@ -132,52 +192,51 @@ export class DataService {
     return promise;
   }
 
- getGamesFixtures(year, round) {
-   const gamesURL = `https://api.squiggle.com.au/?q=games;year=${year};round=${round}`;
-   console.log(gamesURL);
-   const promise = new Promise(
-    (resolve) => {
+  getGamesFixtures(year, round) {
+    const gamesURL = `https://api.squiggle.com.au/?q=games;year=${year};round=${round}`;
+    console.log(gamesURL);
+    const promise = new Promise(
+      (resolve) => {
 
-          this.httpService.get(gamesURL).subscribe(
-            (data: any) => {
-                let gameData = data.games;
-                for(const index in gameData)
-                {
-                  let game = gameData[index];
-                  const gameNew = new Game(
-                    +game.complete,
-                    +game.is_grand_final,
-                    game.tz,
-                    +game.hbehinds,
-                    game.ateam,
-                    +game.winnerteamid,
-                    +game.hgoals,
-                    game.updated,
-                    +game.round,
-                    +game.is_final,
-                    +game.hscore,
-                    +game.abehinds,
-                    game.winner,
-                    +game.ascore,
-                    game.hteam,
-                    game.ateamid,
-                    game.venue,
-                    +game.hteamid,
-                    +game.agoals,
-                    +game.year,
-                    game.date,
-                    +game.id
-                  );
-                  this.gamesFixtures.push(gameNew);
-                }
-                resolve(this.gamesFixtures);
+        this.httpService.get(gamesURL).subscribe(
+          (data: any) => {
+            let gameData = data.games;
+            for (const index in gameData) {
+              let game = gameData[index];
+              const gameNew = new Game(
+                +game.complete,
+                +game.is_grand_final,
+                game.tz,
+                +game.hbehinds,
+                game.ateam,
+                +game.winnerteamid,
+                +game.hgoals,
+                game.updated,
+                +game.round,
+                +game.is_final,
+                +game.hscore,
+                +game.abehinds,
+                game.winner,
+                +game.ascore,
+                game.hteam,
+                game.ateamid,
+                game.venue,
+                +game.hteamid,
+                +game.agoals,
+                +game.year,
+                game.date,
+                +game.id
+              );
+              this.gamesFixtures.push(gameNew);
             }
-          );
-    }
+            resolve(this.gamesFixtures);
+          }
+        );
+      }
 
-  );
-   return promise;
- }
+    );
+    return promise;
+  }
 
 
 
@@ -251,12 +310,12 @@ export class DataService {
     const promise = new Promise(
       (resolve) => {
 
-          this.httpService.get(tipURL).subscribe(
-            (data: any) => {
-              // console.log('TIP DATA -> ', data);
-              resolve(data);
-            }
-          );
+        this.httpService.get(tipURL).subscribe(
+          (data: any) => {
+            // console.log('TIP DATA -> ', data);
+            resolve(data);
+          }
+        );
       }
 
     );
